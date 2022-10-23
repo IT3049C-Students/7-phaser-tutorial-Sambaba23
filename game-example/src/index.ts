54534bac6437dc6df7baa53f018beb917eb07163
Sambaba23 <81830827+Sambaba23@users.noreply.github.com>
2022-10-23 16:37:02 -0400

    completed game

--- game-example/src/index.ts
+++ game-example/src/index.ts
@@ -1,102 +1,58 @@
-import { GameObjects, Scene, Tilemaps } from 'phaser';
+import { Game, Scale, Types, WEBGL } from 'phaser';
 
-import { Player } from '../../classes/player';
-import { Enemy } from '../../classes/enemy';
-import { gameObjectsToObjectPoints } from '../../helpers/gameobject-to-object-point';
+import { Level1, LoadingScene, UIScene } from './scenes';
 
-import { EVENTS_NAME } from '../../consts';
+type GameConfigExtended = Types.Core.GameConfig & {
+  winScore: number;
+};
 
-export class Level1 extends Scene {
-  private player!: Player;
-  private map!: Tilemaps.Tilemap;
-  private tileset!: Tilemaps.Tileset;
-  private wallsLayer!: Tilemaps.DynamicTilemapLayer;
-  private groundLayer!: Tilemaps.DynamicTilemapLayer;
-  private chests!: GameObjects.Sprite[];
-  private enemies!: Enemy[];
+export const gameConfig: GameConfigExtended = {
+  title: 'Phaser game tutorial',
+  type: WEBGL,
+  parent: 'game',
+  backgroundColor: '#351f1b',
+  scale: {
+    mode: Scale.ScaleModes.NONE,
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+    },
+  },
+  render: {
+    antialiasGL: false,
+    pixelArt: true,
+  },
+  callbacks: {
+    postBoot: () => {
+      window.sizeChanged();
+    },
+  },
+  canvasStyle: `display: block; width: 100%; height: 100%;`,
+  autoFocus: true,
+  audio: {
+    disableWebAudio: false,
+  },
+  scene: [LoadingScene, Level1, UIScene],
+  winScore: 40,
+};
 
-  constructor() {
-    super('level-1-scene');
+window.sizeChanged = () => {
+  if (window.game.isBooted) {
+    setTimeout(() => {
+      window.game.scale.resize(window.innerWidth, window.innerHeight);
+
+      window.game.canvas.setAttribute(
+        'style',
+        `display: block; width: ${window.innerWidth}px; height: ${window.innerHeight}px;`,
+      );
+    }, 100);
   }
+};
 
-  create(): void {
-    this.initMap();
-    this.player = new Player(this, 100, 100);
-    this.initChests();
-    this.initEnemies();
-    this.initCamera();
+window.onresize = () => window.sizeChanged();
 
-    this.physics.add.collider(this.player, this.wallsLayer);
-  }
-
-  update(): void {
-    this.player.update();
-  }
-
-  private initMap(): void {
-    this.map = this.make.tilemap({ key: 'dungeon', tileWidth: 16, tileHeight: 16 });
-    this.tileset = this.map.addTilesetImage('dungeon', 'tiles');
-    this.groundLayer = this.map.createDynamicLayer('Ground', this.tileset, 0, 0);
-    this.wallsLayer = this.map.createDynamicLayer('Walls', this.tileset, 0, 0);
-    this.wallsLayer.setCollisionByProperty({ collides: true });
-
-    this.physics.world.setBounds(0, 0, this.wallsLayer.width, this.wallsLayer.height);
-    this.showDebugWalls();
-  }
-
-  private initChests(): void {
-    const chestPoints = gameObjectsToObjectPoints(
-      this.map.filterObjects('Chests', (obj) => obj.name === 'ChestPoint'),
-    );
-
-    this.chests = chestPoints.map((chestPoint) =>
-      this.physics.add.sprite(chestPoint.x, chestPoint.y, 'tiles_spr', 595).setScale(1.5),
-    );
-
-    this.chests.forEach((chest) => {
-      this.physics.add.overlap(this.player, chest, (obj1, obj2) => {
-        this.game.events.emit(EVENTS_NAME.chestLoot);
-        obj2.destroy();
-        // this.cameras.main.flash();
-      });
-    });
-  }
-
-  private initEnemies(): void {
-    const enemiesPoints = gameObjectsToObjectPoints(
-      this.map.filterObjects('Enemies', (obj) => obj.name === 'EnemyPoint'),
-    );
-
-    this.enemies = enemiesPoints.map((enemyPoint) =>
-      new Enemy(this, enemyPoint.x, enemyPoint.y, 'tiles_spr', this.player, 503)
-        .setName(enemyPoint.id.toString())
-        .setScale(1.5),
-    );
-
-    this.physics.add.collider(this.enemies, this.wallsLayer);
-    this.physics.add.collider(this.enemies, this.enemies);
-    this.physics.add.collider(
-      this.player,
-      this.enemies,
-      (obj1, obj2) => {
-        (obj1 as Player).getDamage(1);
-      },
-      undefined,
-      this,
-    );
-  }
-
-  private initCamera(): void {
-    this.cameras.main.setSize(this.game.scale.width, this.game.scale.height);
-    this.cameras.main.startFollow(this.player, true, 0.09, 0.09);
-    this.cameras.main.setZoom(2);
-  }
-
-  private showDebugWalls(): void {
-    const debugGraphics = this.add.graphics().setAlpha(0.7);
-    this.wallsLayer.renderDebug(debugGraphics, {
-      tileColor: null,
-      collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255),
-    });
-  }
-}
+window.game = new Game(gameConfig);
